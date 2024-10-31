d8f4fc45c10b15839bb0fc8788abb48d2350f44f
DevOps-PcNay <pcnay2003@outlook.com>
2024-10-30 19:49:44 -0700

    Pagina de Inicio

---
+++ Web/Views/Pages/Home/Home.php
@@ -0,0 +1,12 @@
+<div class="container">
+	
+<?php 
+		include "Modules/Banner.php"; 
+		include "Modules/Free.php"; 
+		include "Modules/Most-Seen.php";
+		include "Modules/Most-Sold.php";
+		include "Modules/Slide.php"; 
+		include "Modules/Visits.php"; 
+	?>
+
+</div>
