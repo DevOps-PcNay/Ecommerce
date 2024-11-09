525d28bc509f31a5e1bc790b2106bcea473fb559
DevOps-PcNay <pcnay2003@outlook.com>
2024-11-08 20:40:41 -0800

    Se ermina el modulo de Banner

--- Web/Views/Pages/Home/Home.php
+++ Web/Views/Pages/Home/Home.php
@@ -1,14 +1,15 @@
 <div class="container">
 	
-<!-- <h1> PAGINA DE INICIO</h1> -->
+ <!-- <h1> PAGINA DE INICIO</h1>  -->
 
 <?php 
-		include "Modules/Banner.php"; 
+		include "Modules/Slide.php"; 
+		include "Modules/Banner.php"; 		
 		include "Modules/Free.php"; 
 		include "Modules/Most-Seen.php";
-		include "Modules/Most-Sold.php";
-		include "Modules/Slide.php"; 
+		include "Modules/Most-Sold.php";		
 		include "Modules/Visits.php"; 	
-	?>
+		?>
+
 
 </div>
---
+++ Web/Views/Pages/Home/Modules/Banner.php
@@ -0,0 +1,19 @@
+<div class="container-fluid banner" style="position:relative; background: url('<?php echo $path ?>Views/Assets/Img/Banner/1/default.jpg');background-size:cover;background-position:center; background-repeat:no-repeat;">
+
+	<div class="container-fluid" style ="background-color:rgba(0,0,0,.5);">
+
+		<div class="container text-right p-5">
+
+			<h1 class="text-uppercase" style="color:#fff; text-shadow: 2px 2px 5px #000">Ofertas Especiales </h1>
+			<h2  style="color:#fff; text-shadow: 2px 2px 5px #000">50% Off</h2>
+			<h3  style="color:#fff; text-shadow: 2px 2px 5px #000">Termina el 31 De Octubre</h3>
+		</div>
+
+	</div> <!-- <div class="container-fluid" style ="background-color:fgba(0,0,0,0.5);"> 	-->	
+
+</div> <!-- <div class="container-fluid" style="position:relative; backgroud:url( .. -->
+
+<script>
+	// Para el efecto Paradox, (la imagen queda fija cuando se aplica el Scroll.)
+	$(".banner").css({'background-attachment':'fixed'});
+</script>
